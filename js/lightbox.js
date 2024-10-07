2d80f240c54b86e58cb3481458122ec4047fb9d9
dktoby84 <tobias.s.christiansen@gmail.com>
2024-10-07 12:50:00 +0200

    full screen img

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,11 +1,20 @@
 // Open the lightbox
 function openLightbox() {
-  document.getElementById("lightbox").style.display = "block";
+  const lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "flex"; // Use flex to center
+  
+  // Request full-screen for the lightbox itself
+  if (lightbox.requestFullscreen) {
+    lightbox.requestFullscreen();
+  }
 }
 
 // Close the lightbox
 function closeLightbox() {
   document.getElementById("lightbox").style.display = "none";
+  if (document.fullscreenElement) {
+    document.exitFullscreen(); // Exit full-screen mode
+  }
 }
 
 // Change the image in the lightbox
